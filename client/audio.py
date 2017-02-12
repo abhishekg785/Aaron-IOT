b727f298788b35566e486d88ee0b89a396b0fbeb
abhishekg785 <abhishekg785@gmail.com>
2017-02-12 19:21:11 +0530

    AudioHandler class created

--- client/audio.py
+++ client/audio.py
@@ -7,9 +7,29 @@
     such as user audio input and output
 """
 
-class Audio():
+import pyaudio  # handles the record and play of the audio files
+import audioop  # lib for handling math operations on the audio file
+
+class AudioHandler():
 
     def __init__(self):
+        print 'Cons of the AudioHandler Invoked'
+        self.pyaudio = pyaudio.PyAudio()
+
+    """ invokeListener : Is inoked at the time the user says something.
+    I have to use a keyword to determine whether the user wants to convey
+    a command or not or Simply to activate out system
+    I will be using 'Hiro' keyword for the moment
+
+    How to find whether user has said anything or not
+    Or
+    There is any disturbance by the user or not
+    We can achieve this using a threshold value i.e :
+    """
+    def invokeListener(self, KEYWORD):
         pass
+
+
+
 
 
