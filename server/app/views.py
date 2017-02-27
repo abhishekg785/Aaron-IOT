9b8d567ca94cd1d1d4f2aafb083d3aa59608d966
abhishekg785 <abhishekg785@gmail.com>
2017-02-27 07:25:21 +0530

    server created and parser modules added and working fine

--- server/app/views.py
+++ server/app/views.py
@@ -4,9 +4,31 @@
 
     views.py : handling the routes
 """
+
 from app import app
+
+import sys
+
+# adding client to the sys path to get the modules available here
+sys.path.append('/home/hiro/Documents/hiro/client/')
+
+# getting the modules required for processing the query
+from audio import AudioHandler
+from processText import ProcessText
+
+# Instantiate
+audio = AudioHandler()
+parser = ProcessText(audio)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello'
+    return 'hello world'
+
+
+# Route for handling the user query received through the url
+@app.route('/parser/<text>')
+def parseText(text):
+    parser.parseText(text)
+    return 'yo!'
